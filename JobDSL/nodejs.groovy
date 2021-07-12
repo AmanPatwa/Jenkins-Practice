79423efb1cd222c568ef4bce8266bce8dd6f8323
AmanPatwa <amanpatwa999@gmail.com>
2021-07-12 21:25:46 +0530

    git config added

--- JobDSL/nodejs.groovy
+++ JobDSL/nodejs.groovy
@@ -1,6 +1,10 @@
 job('NodeJs Example'){
     scm{
-        git('git://github.com/wardviaene/docker-demo.git')
+        git('git://github.com/wardviaene/docker-demo.git') {
+            node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('AmanPatwa')
+            node / gitConfigEmail('amanpatwa999@gmail.com')
+        }
     }
     triggers{
         scm('H/5 * * * *')
